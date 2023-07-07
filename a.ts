c74ea3dd7099d6219ad4cc36687387cfb0f9de2b
WalterJoel <walter.valdivia@ucsp.edu.pe>
2023-07-07 11:56:10 -0500

    comi

--- a.ts
+++ a.ts
@@ -5,3 +5,6 @@ const c = "tercer commit que pushea en master"
 
 const d = "hago un cambio en RAMA 1 para que cree otra RAMA desde RAMA1 y ver si me trae esto o lo de RAMA MAIN "
 const e = "hago un cambio en RAMA 1 para que cree otra RAMA desde RAMA1 y ver si me trae esto o lo de RAMA MAIN "
+
+
+const f = "hago un cambio en la rama2desderama1 para fusionar con rama1 y luego a main"
